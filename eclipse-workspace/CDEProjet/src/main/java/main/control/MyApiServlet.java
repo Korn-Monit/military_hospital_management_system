4962db4d08c70c983353bb19df3d5f6772815e4c
Korn-Monit <kornmonit@gmail.com>
2024-04-20 19:39:46 +0200

    v8

--- eclipse-workspace/CDEProjet/src/main/java/main/control/MyApiServlet.java
+++ eclipse-workspace/CDEProjet/src/main/java/main/control/MyApiServlet.java
@@ -20,8 +20,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import main.model.Stock;
-
 @WebServlet(name = "MyApiServlet", urlPatterns = {"/api/getStock", "/api/addStock", "/api/updateStock", "/api/deleteStock"})
 public class MyApiServlet extends HttpServlet {
     // Declare a static variable to hold the database connection
@@ -30,11 +28,9 @@ public class MyApiServlet extends HttpServlet {
     private static final String TABLE1_NAME = "stock";
     private static final String TABLE2_NAME = "medicin";
     private static final String TABLE3_NAME = "equipement";
-    Stock stock = new Stock();
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-    	
         // Call the function to establish the database connection
         try {
             connection = establishConnection();
@@ -117,7 +113,7 @@ public class MyApiServlet extends HttpServlet {
         	Connection connection;
 			connection = establishConnection();
 
-           String data = stock.retrieveDataFromDatabase(connection);
+           String data = retrieveDataFromDatabase(connection);
            response.getWriter().append(data);
             
             request.setAttribute("data",data);
@@ -137,7 +133,7 @@ public class MyApiServlet extends HttpServlet {
             String nominal = request.getParameter("nominal");
             String secteur = request.getParameter("secteur");
             String natur_des_colis = request.getParameter("natur_des_colis");
-            String n_du_colis = request.getParameter("n_du_colis");          
+            String n_du_colis = request.getParameter("n_du_colis");
             String designation_generique_du_colis = request.getParameter("designation_generique_du_colis");
             String precision_arti = request.getParameter("precision_arti");
             String dimens_long_large_haut = request.getParameter("dimens_long_large_haut");
@@ -150,7 +146,7 @@ public class MyApiServlet extends HttpServlet {
             String champ_concataine = request.getParameter("champ_concataine");
 
             
-            stock.addDataToStock(affectataire, module, nominal, secteur, natur_des_colis, n_du_colis, designation_generique_du_colis, precision_arti, dimens_long_large_haut,volume,poids,valeur,iata,projection,observation,champ_concataine);
+            addDataToStock(affectataire, module, nominal, secteur, natur_des_colis, n_du_colis, designation_generique_du_colis, precision_arti, dimens_long_large_haut,volume,poids,valeur,iata,projection,observation,champ_concataine);
             response.getWriter().write("Data added successfully!");
         } catch (SQLException e) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
@@ -172,9 +168,55 @@ public class MyApiServlet extends HttpServlet {
     }
 
 
-
+    //function to retrieve data from database
+    private String retrieveDataFromDatabase(Connection connection) throws SQLException {
+//    	System.out.print("haha");
+        try (
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM stock")) 
+        {   	
+            StringBuilder sb = new StringBuilder();
+            while (resultSet.next()) {
+                sb.append(resultSet.getString("module")).append("\n");
+            }
+            return sb.toString();
+        }
+    }
     
+    // Function to add data to the database
+    public void addDataToStock(String test1, String test2, String test3, String test4, String test5, String test6, String test7, String test8, String test9, String test10, String test11, String test12, String test13, String test14, String test15, String test16) throws SQLException {
+        String insertUserSQL = "INSERT INTO stock (affectataire, module, nominal, secteur, natur_des_colis, n_du_colis, designation_generique_du_colis, precision_arti, dimens_long_large_haut, volume, poids, valeur, iata, projection, observation, champ_concataine) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
+        try (Connection connection = establishConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertUserSQL)) {
+
+            // Set the values for the placeholders in the SQL query
+            preparedStatement.setString(1, test1);
+            preparedStatement.setString(2, test2);
+            preparedStatement.setString(3, test3);
+            preparedStatement.setString(4, test4);
+            preparedStatement.setString(5, test5);
+            preparedStatement.setString(6, test6);
+            preparedStatement.setString(7, test7);
+            preparedStatement.setString(8, test8);
+            preparedStatement.setString(9, test9);
+            preparedStatement.setString(10, test10);
+            preparedStatement.setString(11, test11);
+            preparedStatement.setString(12, test12);
+            preparedStatement.setString(13, test13);
+            preparedStatement.setString(14, test14);
+            preparedStatement.setString(15, test15);
+            preparedStatement.setString(16, test16);
+
+            // Execute the SQL statement
+            preparedStatement.executeUpdate();
+
+            System.out.println("Added successfully!");
+        } catch (SQLException e) {
+            System.err.println("Error adding user to the database: " + e.getMessage());
+            throw e; // Rethrow the exception if you want the caller to handle it
+        }
+    }
     
 //   update stock
     protected void updateDataInDatabase(int id, String nominal) {
